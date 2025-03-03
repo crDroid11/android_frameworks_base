45b5ad1cd9b514db844e84dad65794fa7bea6bbd
Julia Reynolds <juliacr@google.com>
2025-03-03 06:28:05 -0800

    Revert "[BACKPORT] Check sound Uri permission when creating a notification channel"
    This reverts commit 5480c2b933dda9199f7856cd445a8c759c5969e6.
    
    Reason for revert: b/337775777
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:2d9506053e521542fc6b8127f44c9d76897904c1)
    Merged-In: Ib569f25a616eb13439be177901fdb78fa02f539a
    Change-Id: Ib569f25a616eb13439be177901fdb78fa02f539a

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -2083,7 +2083,6 @@ public class NotificationManagerService extends SystemService {
                 mZenModeHelper,
                 new NotificationChannelLoggerImpl(),
                 mAppOps,
-                mUgmInternal,
                 new SysUiStatsEvent.BuilderFactory());
         mRankingHelper = new RankingHelper(getContext(),
                 mRankingHandler,
@@ -5253,8 +5252,7 @@ public class NotificationManagerService extends SystemService {
             final Uri soundUri = updateChannel.getSound();
             final Uri originalSoundUri =
                     (originalChannel != null) ? originalChannel.getSound() : null;
-            if (soundUri != null && !Objects.equals(originalSoundUri, soundUri) &&
-                ContentResolver.SCHEME_CONTENT.equals(soundUri.getScheme())) {
+            if (soundUri != null && !Objects.equals(originalSoundUri, soundUri)) {
                 Binder.withCleanCallingIdentity(() -> {
                     mUgmInternal.checkGrantUriPermission(sourceUid, null,
                             ContentProvider.getUriWithoutUserId(soundUri),
